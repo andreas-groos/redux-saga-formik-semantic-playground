d7a7f3d351964a42fc3555995cac4f51572eb9f5
Andreas <andreas.groos1@gmail.com>
2019-09-29 11:02:56 -0700

    Unstyled Formik form

--- app/components/InputForm.js
+++ app/components/InputForm.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Form } from 'semantic-ui-react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+export const DisplayFormikState = props => (
+  <div style={{ margin: '1rem 0' }}>
+    <h3 style={{ fontFamily: 'monospace' }} />
+    <pre
+      style={{
+        background: '#f6f8fa',
+        fontSize: '.65rem',
+        padding: '.5rem',
+      }}
+    >
+      <strong>props</strong> = {JSON.stringify(props, null, 2)}
+    </pre>
+  </div>
+);
 
 function InputForm(props) {
   const handleSubmit = e => {
@@ -13,20 +30,87 @@ function InputForm(props) {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group>
-        <Form.Input
-          onChange={handleChange}
-          name="firstName"
-          label="First name"
-          placeholder="First Name"
-          width={6}
-          // error="nononononon"
-        />
-        <Form.Input label="Last Name" placeholder="Last Name" width={10} />
-        <Form.Input label="Email" placeholder="Last Name" width={8} />
-      </Form.Group>
-    </Form>
+    <div>
+      <Formik
+        initialValues={{ email: '' }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+        validationSchema={Yup.object().shape({
+          email: Yup.string()
+            .email()
+            .required('Required'),
+        })}
+      >
+        {props => {
+          const {
+            values,
+            touched,
+            errors,
+            dirty,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            handleReset,
+          } = props;
+          return (
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="email" style={{ display: 'block' }}>
+                Email
+              </label>
+              <input
+                id="email"
+                placeholder="Enter your email"
+                type="text"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={
+                  errors.email && touched.email
+                    ? 'text-input error'
+                    : 'text-input'
+                }
+              />
+              {errors.email && touched.email && (
+                <div className="input-feedback">{errors.email}</div>
+              )}
+
+              <button
+                type="button"
+                className="outline"
+                onClick={handleReset}
+                disabled={!dirty || isSubmitting}
+              >
+                Reset
+              </button>
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+
+              <DisplayFormikState {...props} />
+            </form>
+          );
+        }}
+      </Formik>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Input
+            onChange={handleChange}
+            name="firstName"
+            label="First name"
+            placeholder="First Name"
+            width={6}
+            // error="nononononon"
+          />
+          <Form.Input label="Last Name" placeholder="Last Name" width={10} />
+          <Form.Input label="Email" placeholder="Last Name" width={8} />
+        </Form.Group>
+      </Form>
+    </div>
   );
 }
 
